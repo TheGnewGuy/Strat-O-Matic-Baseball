49086e8a07d91a3290675931620c42a6fa4e70bf
TheGnewGuy <john.gnew@verizon.net>
2014-04-09 12:05:40 -0500

    Corrected compiler warnings

--- StdAfx.h
+++ StdAfx.h
@@ -12,6 +12,8 @@
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
+#include "targetver.h"
+
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
 #include <afxdisp.h>        // MFC OLE automation classes
